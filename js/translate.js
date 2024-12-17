7594f8515dbbc4232445249ede30811e60388100
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-16 17:07:21 -0800

    changed the background for select langauge to darkmode/black

--- js/translate.js
+++ js/translate.js
@@ -7,35 +7,13 @@ function googleTranslateElementInit() {
 //Reference:
 // https://www.w3schools.com/howto/howto_google_translate.asp
 
-// attempt to change google translate menu button to darkmode:
 
-// $('document').ready(function () {
-//   $('#google_translate_element').on("click", function () {
-
-//       // Change font family and color
-//       $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
-//           .css({
-//               'color': '#687074',
-//               'font-family': 'tahoma'
-//           });
-
-//       // Change hover effects
-//       $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
-//           $(this).css('background-color', '#18BA9B').find('span.text').css('color', 'black');
-//       }, function () {
-//           $(this).css('background-color', 'black').find('span.text').css('color', '#687074');
-//       });
-
-//       // Change Google's default blue border
-//       $("iframe").contents().find('.VIpgJd-ZVi9od-xl07Ob-OEVmcd').css('border', '1px solid #18BA9B');
-
-//       // Change the iframe's box shadow
-//       $(".VIpgJd-ZVi9od-xl07Ob-OEVmcd").css({
-//           '-moz-box-shadow': '0 3px 8px 2px #666666',
-//           '-webkit-box-shadow': '0 3px 8px 2px #666',
-//           'box-shadow': '0 3px 8px 2px #666',
-//           'background-color': 'rgb(0, 0, 0)'
-//       });
-//   });
-// });
-
+// DARKmode 'Select Langauge' menu
+const run = () => {
+  let frame = document.querySelector(".VIpgJd-ZVi9od-xl07Ob-OEVmcd")
+  const styleTag = document.createElement("style");
+  styleTag.textContent = "body *{ background-color: black !important; color: white !important; }";
+  frame.contentDocument.head.appendChild(styleTag);
+}
+// 1 second 
+setTimeout(run, 1000);
